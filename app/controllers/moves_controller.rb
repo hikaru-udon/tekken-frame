79462d5fdbd5505134372268f88822affd63f920
hikaru-udon <mare.nozomu512@gmail.com>
2021-04-30 11:16:32 +0900

    トラッシュの訂正

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,29 +1,10 @@
 class MovesController < ApplicationController
-<<<<<<< Updated upstream
 # before_action :set_move, only: [:index, :show]
 
-def index
-  # binding.pry
-  @character = Character.find(params[:character_id])
-  @moves = @character.moves
-end
-
-  private
-
-  # def set_move
-  # end
-=======
-  before_action :set_move
-
   def index
-
+    # binding.pry
+    @character = Character.find(params[:character_id])
+    @moves = @character.moves
   end
 
-  private
-
-  def set_move
-    
-  end
-
->>>>>>> Stashed changes
 end
