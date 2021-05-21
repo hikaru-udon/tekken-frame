662cd0f39a204cc7dc21ffb2fb50fc6f7d7d6ac0
hikaru-udon <mare.nozomu512@gmail.com>
2021-05-21 13:10:30 +0900

    Revert "ルーティングsearch"
    This reverts commit d5f85297574d68535f0f800ac1d6899fac102ad3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root "characters#index"
   resources :characters do
-    resources :moves, only: [:index, :search]
-    collection do
-      get 'search'
-    end
+    resources :moves
   end
 end
