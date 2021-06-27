55ee6b28c56f73a3955ef6dd9828dd20edc4e656
hikaru-udon <mare.nozomu512@gmail.com>
2021-06-27 13:01:12 +0900

    各所ビューの整理

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,3 +1,15 @@
 class Move < ApplicationRecord
   belongs_to :character
+<<<<<<< Updated upstream
+=======
+
+  def self.search(search) #self.はMove.を意味する
+    if search !=""
+      Move.where(['moves_name LIKE ? or startup LIKE ? or block LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"]) #キーワード検索で技名、発生F、ガードFの部分一致を表示。
+    else
+      Move.all #全て表示させる
+    end
+  end
+
+>>>>>>> Stashed changes
 end
