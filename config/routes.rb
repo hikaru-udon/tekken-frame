1d0d36ecbb741a09276d1cffb7094cb28a4e259a
hikaru-udon <mare.nozomu512@gmail.com>
2021-07-04 11:08:08 +0900

    showアクションへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "characters#index"
   resources :characters do
-    resources :moves, only: :index do
+    resources :moves, only: [:index, :show] do
     collection do
       get 'search'
     end
