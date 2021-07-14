89250249fe170d2e281f34c9d809b088333c77ac
hikaru-udon <mare.nozomu512@gmail.com>
2021-07-14 14:54:44 +0900

    行をリンクにする複数案としてJSを仮実装
    ・非同期通信の実装で使うかもしれないので一応ファイルは保存

---
+++ app/javascript/moves/show.js
@@ -0,0 +1,9 @@
+// //jQuery.js（もしくは対象HTMLの<script>タグ内に記述）
+// $(function($) {
+//   //moves-contentsクラスの行をマウスオーバーするとカーソルをポインターに変更
+//      $(".moves-contents").css("cursor","pointer").click(function() {
+//   //クリックで「data-href」のリンク先へ遷移
+//          window.href = $(this).data("href");
+//          console.log(this)
+//      });
+//  });
